3378d468ce1f8f5b8900239ad1e4565867d4fea3
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2022-11-17 11:27:00 +0000

    initial commit

---
+++ pre_commit_hooks/detect_consecutive_blank_lines.py
@@ -0,0 +1,17 @@
+import argparse
+
+def main(argv) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args(argv)
+
+    retcode = 0
+    for filename in args.filenames:
+        with open(filename, 'rb') as inputfile:
+            print(f'{filename}')
+
+    return retcode
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
